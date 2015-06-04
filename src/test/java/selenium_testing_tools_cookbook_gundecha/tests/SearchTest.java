ecf173ee7fed8cc204294b85228940b0ef4da4bd
David Forshaw <dfcv126@gmail.com>
2015-06-03 20:58:00 -0700

    1st files for new book

---
+++ src/test/java/selenium_testing_tools_cookbook_gundecha/tests/SearchTest.java
@@ -0,0 +1,27 @@
+package selenium_testing_tools_cookbook_gundecha.tests;
+
+// page 156
+import selenium_testing_tools_cookbook_gundecha.pageobjects.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SearchTest {
+
+    @Test
+    public void testProductSearch() {
+
+        HomePage homePage = new HomePage();
+
+        homePage.get();
+
+        SearchResults searchResult = homePage.Search().searchInStore("Sony");
+
+        assertEquals(2, searchResult.getProducts().size());
+        assertTrue(searchResult.getProducts().contains("Sony Ericsson W810i"));
+
+        searchResult.close();
+
+    }
+
+}
